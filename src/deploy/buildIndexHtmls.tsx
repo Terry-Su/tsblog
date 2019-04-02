6a6f18b57811050b468037678831204565ba0fff
TerrySu <2684759855@qq.com>
2019-04-02 20:23:02 +0800

    consider the special situation in development

--- src/deploy/buildIndexHtmls.tsx
+++ src/deploy/buildIndexHtmls.tsx
@@ -56,41 +56,54 @@ window.${key}=${serializeJavascript(windowData[key], { unsafe: true })}
     }
 
     const { reduxApp, title } = config.entry
-    const useRedux = !!reduxApp
-    const reducer = useRedux ? require(reduxApp).default : {}
-
-    const App = require(PATH_CACHE_APP_COMPONENT).default
     const Loadable = require(PATH_TARGET_REACT_LOADABLE)
-    const { ServerStyleSheet } = require(PATH_TARGET_STYLED_COMPONENTS)
-    const { createStore } = require(PATH_TARGET_REDUX)
-    const { Provider } = require(PATH_TARGET_REACT_REDUX)
+
+    let App
+    let ServerStyleSheet
+    let useRedux
+    let Provider
+    let createStore
+    let reducer
+    if (!__DEV__) {
+      useRedux = !!reduxApp
+      reducer = useRedux ? require(reduxApp).default: {}
+      App = require(PATH_CACHE_APP_COMPONENT).default
+      ServerStyleSheet = require(PATH_TARGET_STYLED_COMPONENTS).ServerStyleSheet
+      createStore = require(PATH_TARGET_REDUX).createStore
+      Provider = require(PATH_TARGET_REACT_REDUX).Provider
+    }
+
     // preload all loadable components first
     await Loadable.preloadAll().then(() => {
       let modules = []
       const stats = require(PATH_PUBLIC_LOADABLE)
+      let appHtml = ""
+      let style = ""
 
-      const sheet = new ServerStyleSheet()
+      if ( ! __DEV__ ) {
+        const sheet = new ServerStyleSheet()
 
-      const main = (
-        <StaticRouter location={path} context={{}}>
-          <Loadable.Capture report={moduleName => modules.push(moduleName)}>
-            <App />
-          </Loadable.Capture>
-        </StaticRouter>
-      )
-
-      const appHtml = __DEV__
-        ? ""
-        : ReactDOMServer.renderToString(
-            sheet.collectStyles(
-              useRedux ? (
-                <Provider store={createStore(reducer)}>{main}</Provider>
-              ) : (
-                main
+        const main = (
+          <StaticRouter location={path} context={{}}>
+            <Loadable.Capture report={moduleName => modules.push(moduleName)}>
+              <App />
+            </Loadable.Capture>
+          </StaticRouter>
+        )
+  
+         appHtml = ReactDOMServer.renderToString(
+              sheet.collectStyles(
+                useRedux ? (
+                  <Provider store={createStore(reducer)}>{main}</Provider>
+                ) : (
+                  main
+                )
               )
             )
-          )
-      const style = sheet.getStyleTags()
+          style = sheet.getStyleTags()
+      }
+     
+      
       const globalStyle = `<style>
 html,body,#root {
   width: 100%;
